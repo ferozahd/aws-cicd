be4f32808d0645e57add5391fcdcf29367f74909
Feroz Ahmmed <feroz.shah.940@gmail.com>
2024-12-19 13:47:35 +0600

    inital: Git inital

---
+++ src/main/java/com/awsdeploy/cicd/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.awsdeploy.cicd.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    ResponseEntity<Map<String,String>> getWelcomeMessage(){
+        Map<String, String> message =new HashMap<>(2);
+        message.put("message","First deploy");
+        message.put("version","deploy at 2024-12-19");
+
+        return ResponseEntity.ok( message);
+    }
+}

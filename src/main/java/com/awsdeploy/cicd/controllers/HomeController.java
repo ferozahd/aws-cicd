33458fe2de7d9ee1afcf2092919a0fac731a801a
Feroz Ahmmed <feroz.shah.940@gmail.com>
2024-12-19 14:24:46 +0600

    refactor

--- src/main/java/com/awsdeploy/cicd/controllers/HomeController.java
+++ src/main/java/com/awsdeploy/cicd/controllers/HomeController.java
@@ -3,7 +3,6 @@ package com.awsdeploy.cicd.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.HashMap;
 import java.util.Map;
 
